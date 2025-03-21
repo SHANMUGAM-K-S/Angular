bdcdd942b3bca45b9e585da6414b751c969791bd
SHANMUGAM-K-S <magizhchisk@gmail.com>
2025-03-21 19:42:26 +0530

    Updated project with new features/fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,22 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { TemporaryComponent } from "./temporary/temporary.component";
 import { ShanmugamComponent } from "./shanmugam/shanmugam.component";
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, TemporaryComponent, ShanmugamComponent],
+  imports: [LoginComponent, TemporaryComponent, ShanmugamComponent,ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular';
+ 
+    handleClick(){
+alert("Function Called")
+  }
+  handleClicking(){
+    console.log("Button Clicked");
+    
+  }
 }
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,19 @@
+// we can customize our code whatever we want
+
+
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'profile',
+    imports:[],
+    templateUrl:'./profile.component.html',
+    // template:`<h1>Profile Component</h1>`,
+    // styles:`h1{color:green}`
+    styleUrl:'./profile.component.css'
+
+})
+
+
+export class ProfileComponent {
+     
+}
